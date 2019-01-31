5e55ca309ab9b1b4f7d6520f7578f9807eceff7a
Nick Bennett <nick@nickbenn.com>
2019-01-31 13:35:59 -0700

    Initial commit

---
+++ src/edu/cnm/deepdive/Card.java
@@ -0,0 +1,41 @@
+package edu.cnm.deepdive;
+
+/**
+ * Instances of this class represent single cards in a deck of standard playing
+ * cards. These instances are immutable: once initialized, the rank and suit of
+ * a card can't be changed.
+ *
+ * @author Nicholas Bennett &amp; Deep Dive Coding Java + Android Bootcamp cohort 6
+ * @version 1.0
+ */
+public class Card {
+
+  private final Rank rank;
+  private final Suit suit;
+
+  /**
+   * Initializes the <code>Card</code> instance with the specified {@link
+   * Suit} and {@link Rank}.
+   *
+   * @param rank  card's rank (A, 2, &hellip; K).
+   * @param suit  card's suit (Clubs, Diamonds, Hearts, Spades).
+   */
+  public Card(Rank rank, Suit suit) {
+    this.rank = rank;
+    this.suit = suit;
+  }
+
+  public Rank getRank() {
+    return rank;
+  }
+
+  public Suit getSuit() {
+    return suit;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("%s %s", rank, suit);
+  }
+
+}
---
+++ src/edu/cnm/deepdive/Suit.java
@@ -0,0 +1,29 @@
+package edu.cnm.deepdive;
+
+/**
+ * This enum implements suits of standard playing cards. Each enumerated value
+ * uses the standard symbol for that suit (taken from Unicode), as its string
+ * representation.
+ *
+ * @author Nicholas Bennett &amp; Deep Dive Coding Java + Android Bootcamp cohort 6
+ * @version 1.0
+ */
+public enum Suit {
+
+  CLUBS("C"),
+  DIAMONDS("D"),
+  HEARTS("H"),
+  SPADES("S");
+
+  private final String symbol;
+
+  Suit(String symbol) {
+    this.symbol = symbol;
+  }
+
+  @Override
+  public String toString() {
+    return symbol;
+  }
+
+}
