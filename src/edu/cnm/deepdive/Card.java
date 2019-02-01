c21d4fa5ff840d685ebf602d05d24d194c1630b3
Nick Bennett <nick@nickbenn.com>
2019-02-01 10:35:04 -0700

    Implemented card trick.

--- src/edu/cnm/deepdive/Card.java
+++ src/edu/cnm/deepdive/Card.java
@@ -8,7 +8,7 @@ package edu.cnm.deepdive;
  * @author Nicholas Bennett &amp; Deep Dive Coding Java + Android Bootcamp cohort 6
  * @version 1.0
  */
-public class Card {
+public class Card implements Comparable<Card> {
 
   private final Rank rank;
   private final Suit suit;
@@ -36,6 +36,15 @@ public class Card {
   @Override
   public String toString() {
     return String.format("%2s %s", rank, suit);
+  }
+
+  @Override
+  public int compareTo(Card other) {
+    int comparison = getSuit().compareTo(other.getSuit());
+    if (comparison == 0) {
+      comparison = getRank().compareTo(other.getRank());
+    }
+    return comparison;
   }
 
 }
